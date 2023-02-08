3d9a21a8d0fe42772ef807b280016839873f2edb
MehediHasan4321 <126mehedi@gmail.com>
2023-02-08 16:59:52 +0600

    emplement delete fucntion

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-btn");
 const todoList = document.querySelector(".todo-list")
 
+
 //Event listner
 todoButton.addEventListener("click", todoAdd)
+
 //Functions
 function todoAdd(event) {
     event.preventDefault()
@@ -16,7 +18,7 @@ function todoAdd(event) {
     todoDiv.appendChild(para)
     //todo li
     const newTodo = document.createElement("li");
-    newTodo.classList.add("todo-item")
+   // newTodo.classList.add("todo-item")
 
     const completBtn = document.createElement("button");
     completBtn.innerHTML= '<i class="fa-solid fa-check"></i>';
@@ -26,9 +28,19 @@ function todoAdd(event) {
     trushBtn.innerHTML='<i class="fa-solid fa-trash"></i>';
     trushBtn.classList.add("trush-btn");
     
+    trushBtn.addEventListener("click",function(){
+        todoList.removeChild(todoDiv)
+    })
+    
     newTodo.appendChild(completBtn);
     newTodo.appendChild(trushBtn);
     todoDiv.appendChild(newTodo);
     todoList.appendChild(todoDiv)
     todoInput.value = ""
+}
+
+function deletecheck(e){
+    
+   console.log(e.target.classList)
+    
 }
