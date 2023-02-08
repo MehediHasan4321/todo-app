221eb99a14fb1e50ea5541f41d94a8377c90cc34
MehediHasan4321 <126mehedi@gmail.com>
2023-02-08 12:04:16 +0600

    fix some bug

--- index.js
+++ index.js
@@ -10,16 +10,22 @@ function todoAdd(event) {
     event.preventDefault()
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
+    const para = document.createElement("p");
+    para.innerText= todoInput.value;
+    para.classList.add("todo-text")
+    todoDiv.appendChild(para)
     //todo li
     const newTodo = document.createElement("li");
     newTodo.classList.add("todo-item")
-    newTodo.innerText = todoInput.value;
+
     const completBtn = document.createElement("button");
-    completBtn.classList.add("complet-btn");
     completBtn.innerHTML= '<i class="fa-solid fa-check"></i>';
+    completBtn.classList.add("complet-btn");
+
     const trushBtn = document.createElement("button");
     trushBtn.innerHTML='<i class="fa-solid fa-trash"></i>';
     trushBtn.classList.add("trush-btn");
+    
     newTodo.appendChild(completBtn);
     newTodo.appendChild(trushBtn);
     todoDiv.appendChild(newTodo);
